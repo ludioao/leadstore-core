6956e0038e928906bcde49265de21f62bd2d55e3
Ludio <ludio.ao@gmail.com>
2019-01-25 21:16:25 -0300

    Melhoria dashboard

--- database/migrations/2019_01_24_000000_leadstore_blog_schema.php
+++ database/migrations/2019_01_24_000000_leadstore_blog_schema.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class LeadstoreOrdersSchema extends Migration
+class LeadstoreBlogSchema extends Migration
 {
 
     public function up()
