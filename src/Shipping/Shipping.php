3277cc35179ec69b2d8e0474d0416abd2015fe97
Ludio <ludio.ao@gmail.com>
2019-05-23 23:59:41 -0400

    add function for json render

--- src/Shipping/Shipping.php
+++ src/Shipping/Shipping.php
@@ -8,4 +8,30 @@ abstract class Shipping
 {
     use ShippingUtils;
     abstract public function process($cartProducts);
+
+
+    /**
+     * @var bool
+     */
+    protected $rendered = false;
+
+    /**
+     * @return bool
+     */
+    public function isRendered(): bool
+    {
+        return $this->rendered;
+    }
+
+    /**
+     * @param bool $rendered
+     *
+     * @return $this
+     */
+    public function setRendered(bool $rendered = true)
+    {
+        $this->rendered = $rendered;
+        return $this;
+    }
+
 }
